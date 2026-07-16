d6b51f61e6d1cd799583eaae72697708d461a75b
dyods-prog <d.yods@inapp-ropriate.com>
2026-07-16 15:50:09 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ const HAPTIC_TRIGGER = "haptic";
 const HAPTIC_START_ACTION = "start";
 const HAPTIC_STOP_ACTION = "stop";
 const HAPTIC_PULSE_DURATION = 45;
-const BUILD_VERSION = "css31-js28";
+const BUILD_VERSION = "css32-js29";
 
 applyDeviceLayout();
 initDebugOverlay();
